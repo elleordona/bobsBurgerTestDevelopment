d369e2501733ee1c6536c2fcd1ba7822b8acb934
Elle Ordona <eto0507@hotmail.co.uk>
2023-01-12 15:38:28 +0000

    clean up of testing code

--- test/numberOfItemsInBasket.spec.js
+++ test/numberOfItemsInBasket.spec.js
@@ -2,29 +2,30 @@
 const { assertEquals } = require(`../test-framework.js`);
 const { basket } = require(`../src/basket.js`);
 
-//initialize variables
-let input = []; 
-let expected, actual, result; //undefined
+//initialize variables 
+let expected, actual, result, item; //undefined
 const myBasket = new basket(); //create an empty basket
 
 //Test One
-console.log('Adding an item => array size +1'); //stating what the test to be carried out is
+console.log('======= Test One =======');
+console.log('addItem adds item into basket and increases the length by 1'); //stating what the test to be carried out is
 
 //Arrange
 expected = 1; //expected value from test
-input = "bagel"; //input the name of the item
+item = "bagel"; //input the name of the item
 
 //Act
-actual = myBasket.addItem(input); //testing the addItem function 
-console.log(actual); // print the value that is calculated
+actual = myBasket.addItem(item); //testing the addItem function 
+//console.log(actual); // print the value that is calculated (can hide this value)
 
 //Assert
-result = assertEquals(actual, expected); //compares expected and actual output
-console.log(`Adding an item => array size +1: ${result ? `Passed` : `Failed`}`); //shows the user if the test passed of failed
-console.log(myBasket.basketItems); //logging the contents of the basket
+result = assertEquals(expected, actual); //compares expected and actual output
+console.log(`${result ? `Passed` : `Failed`}`); //shows the user if the test passed of failed
+//console.log(myBasket.basketItems); //logging the contents of the basket (can hide this value)
 
 //Clean Up 
 input = []; //resetting all the variables for a new test
 expected = undefined;
 actual = undefined;
 result = undefined;
+console.log('======= Test One Done =======');
